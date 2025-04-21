88f94893c42e66cef4a106e414b908a3d7d690ee
Hugo Babin <hugo.babin@gmail.com>
2025-04-21 17:01:47 +0200

    social networks links in footer

--- src/components/ContactFooter.jsx
+++ src/components/ContactFooter.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import addressIcon from '../assets/images/icons/address_icon.png'
 import emailIcon from '../assets/images/icons/email_icon.png'
+import facebookIcon from '../assets/images/icons/facebook_icon.png'
+import instagramIcon from '../assets/images/icons/instagram_icon.png'
+import mariagesNetIcon from '../assets/images/icons/mariages.net_icon.png'
 import phoneIcon from '../assets/images/icons/phone_icon.png'
+import { FACEBOOK_URL, INSTAGRAM_URL, MARIAGE_NET_URL } from "../constants";
 
 function ContactFooter() {
     return (
@@ -26,6 +30,17 @@ function ContactFooter() {
             <div className="contact-information-line">
                 <img src={addressIcon} alt="" />
                 <span>Le Haut Fousseau 53500 Saint-Pierre des Landes</span>
+            </div>
+            <div className="social-networks-line">
+                <a href={MARIAGE_NET_URL}>
+                    <img src={mariagesNetIcon} alt="" />
+                </a>
+                <a href={INSTAGRAM_URL}>
+                    <img src={instagramIcon} alt="" />
+                </a>
+                <a href={FACEBOOK_URL}>
+                    <img src={facebookIcon} alt="" />
+                </a>
             </div>
         </footer>
 
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const FACEBOOK_URL = 'https://www.facebook.com/p/Le-Domaine-du-Haras-de-La-Chaine-100054275179925'
+export const INSTAGRAM_URL = 'https://www.instagram.com/domaineduharasdelachaine'
+export const MARIAGE_NET_URL = 'https://www.mariages.net/domaine-mariage/le-domaine-du-haras-de-la-chaine--e169070'
